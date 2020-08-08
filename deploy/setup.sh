344ce1e98ea0d2f06f1648e2ffa9062c7136b4be
Chinmay-395 <chinmaydali395@gmail.com>
2020-08-08 21:31:21 +0530

    Latest production

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/Chinmay-395/Sepsis-Website.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/Sepsis-Website'
 
 echo "Installing dependencies..."
 apt-get update
