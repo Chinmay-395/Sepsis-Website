9ea9248ff885143cf03bd9e79837a66b0e7ca41e
Chinmay-395 <chinmaydali395@gmail.com>
2020-12-07 23:15:39 +0530

    tests for sending and receving broadcast messages through channel_layer and groups is done

--- Website/backend/src/sepsisDynamic/consumers.py
+++ Website/backend/src/sepsisDynamic/consumers.py
@@ -2,8 +2,48 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class SepsisDynamicConsumer(AsyncJsonWebsocketConsumer):
+
+    groups = ['test']
+
     async def connect(self):
+        """[summary]
+        • [Understanding of django-channels groups methodology]
+            ◘ self.channel_layer.group_add("gossip", self.channel_name) takes
+            two arguments: room_name and channel_name
+            ◘ When you connect from your browser via socket to this consumer, you are
+            creating a new socket connection called as channel. 
+            ◘ So, when you open multiple pages in your browser, multiple channels are created. 
+            Each channel has a unique Id/name : channel_name
+                ♦ The "channel_name" would be provided by django-channel which has to be
+                name must be a valid unicode string containing only ASCII with
+                alphanumerics, hyphens, underscores, or periods.
+            ◘ room is a group of channels. If anyone sends a message to the room, 
+            all channels in that room will receive that message.
+        """
+        await self.channel_layer.group_add(
+            group='test',
+            channel=self.channel_name
+        )
+        print("THE CHANNEL NAME IS ------------> ", self.channel_name)
         await self.accept()
 
     async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            group='test',
+            channel=self.channel_name
+        )
         await super().disconnect(code)
+
+    async def echo_message(self, message):
+        await self.send_json({
+            'type': message.get('type'),
+            'data': message.get('data'),
+        })
+
+    async def receive_json(self, content, **kwargs):
+        message_type = content.get('type')
+        if message_type == 'echo.message':
+            await self.send_json({
+                'type': message_type,
+                'data': content.get('data'),
+            })
--- Website/backend/src/sepsisDynamic/tests/test_websocket.py
+++ Website/backend/src/sepsisDynamic/tests/test_websocket.py
@@ -1,5 +1,6 @@
 import pytest
 from channels.testing import WebsocketCommunicator
+from channels.layers import get_channel_layer
 
 from sepsisWebsite.routing import application
 
@@ -14,6 +15,12 @@ TEST_CHANNEL_LAYERS = {
 @pytest.mark.asyncio
 class TestWebSocket:
     async def test_can_connect_to_server(self, settings):
+        """[summary]
+        • This test will send the will create a variable called
+        `communicator` which would call the consumer of SepsisDynamic
+        • The `communicator` will call the inbuild `connect` 
+        function of Django-Channels to send a connection establishment request 
+        """
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
         communicator = WebsocketCommunicator(
             application=application,
@@ -21,4 +28,56 @@ class TestWebSocket:
         )
         connected, _ = await communicator.connect()
         assert connected is True
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_messages(self, settings):
+        """[summary]
+        • Once the connection established we will send a json data
+        using the inbuild function of django-Channels called `send_json_to` 
+        with the json(data) of `type` & `data` as their key.
+        The `Type` ---> would determine which function to call
+        The `Data` ---> would be the actual data
+        • If the data has been sent succesfully, the function(described in `Type`)
+        would return some json data which would captured by
+        `receive_json_from` inbuilt function of django-channels
+        """
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/sepsisDynamic/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        """[Note]
+        • the `channel layer` is used to broadcast a message to a group. 
+        Whereas the last test modeled a user talking to himself in an empty room, 
+        this most recent test represents a user talking to a room full of people.
+        """
+        """[summary]
+        In this we are using channel-layer and groups to send & receive
+        broadcasted data; this is very similar to upper test.
+        """
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/sepsisDynamic/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
         await communicator.disconnect()
