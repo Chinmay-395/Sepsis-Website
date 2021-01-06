c756bb476b43e1976f35c74266312173e707bad2
Chinmay-395 <chinmaydali395@gmail.com>
2021-01-06 17:36:37 +0530

    Added a short table of the patient on the homePage

--- Website/frontend/gui/src/components/pat_Components/PatHomePage.js
+++ Website/frontend/gui/src/components/pat_Components/PatHomePage.js
@@ -9,46 +9,54 @@ import {fetchPatData} from "../../redux/ActionCreator";
 
 const get_user_type_id = localStorage.getItem('user_type_id');
 
-function TabulatedData(values){
-  return (
-        <Table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </Table>
-      );
-}
-
 function RenderPatientItem({data}){
-  console.log("THE ARRAY",data)
+  if(data!=null || data!==undefined){
+  const returnThis = data.reverse().slice(0,10).map((obj,index)=> {
+    return(
+      <tbody key={index}>
+        <tr>
+          <td>{index+1}</td>
+          <td>{obj.heart_rate}</td>
+          <td>{obj.oxy_saturation}</td>
+          <td>{obj.temperature}</td>
+          <td>{obj.blood_pressure}</td>
+          <td>{obj.mean_art_pre}</td>
+          <td>{obj.resp_rate}</td>
+        </tr>
+      </tbody>
+    )
+  });
   return (
-    <h1>It works</h1>
-  )
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Heart Rate</th>
+          <th>Oxy Saturation</th>
+          <th>temperature</th>
+          <th>blood Pressure</th>
+          <th>Mean Artial Pressure</th>
+          <th>Respiratory Rate</th>
+        </tr>
+      </thead>
+      {returnThis}
+    </Table>
+  )}
+  else {return(<></>)}
 }
 
 function PatHomePage(props) {
-  console.log("PROPS PROPS IN PATHOMEPAGE",props)
+
   useEffect(() => {
-    console.log("THE UseEffct ran")
     props.fetchPatData(get_user_type_id)
-    console.log("WHEN DATA IS MOUNTED",props)
   },[])
 
-  console.log("PROPS PROPS IN PATHOMEPAGE right after useEffect \n \n",props)
-  if(props.patient_data.isLoading)return(<LoadingComponent/>)
+  if (props.patient_data.isLoading)return(<LoadingComponent/>)
   else if(props.patient_data.pat_data !==null){
-    console.log("THE ARRAY OUTSIDE ARROW FUNC",props.patient_data.pat_data.sep_data);
     const theArray = props.patient_data.pat_data.sep_data
-    // RenderPatientItem(theArray)
     return(
     <>
-      <h1>THE ARROW FUNC</h1>
+      <h2>The Latest value of sepsis</h2>
       {<RenderPatientItem data={theArray}/>}
     </>
     )
@@ -67,12 +75,10 @@ const mapStateToProps = (state) => {
   console.log("THE STATE HAS SHIT", state);
   return {
     auth: state.auth,
-    patient_data: state.pat_data,//{ isLoading: false },
+    patient_data: state.pat_data,
   };
 };
 const mapDispatchToProps = (dispatch) => ({
   fetchPatData: (pat_id) => dispatch(fetchPatData(pat_id)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(PatHomePage);
-
-// const theArray = this.props.doctor_data.doc_data.each_pat_json;
